a47a8aab6c893a7c6f833917fef7fab7ca23cd43
mu5t <60688726+mu5t@users.noreply.github.com>
2020-03-12 12:14:14 +0000

    Add function() estructure

--- usergen.sh
+++ usergen.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 echo "Username generator for Kerberos Pentesting"
-echo " "
+echo ""
 
-# Recoge el documento que el usuario pasa como primer argumento
 filename=$1
 
-# Lee cada linea del documento
-while read line;
-  do
-  # Imprime cada linea
-  echo $line
+readeachline() {
+	while read line; do
+		echo $line
+	done < $filename
+}
 
-done < $filename
+readeachline
